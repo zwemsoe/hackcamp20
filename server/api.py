b763191b8e57324f22bf9371d5831247e1d64da7
mukhiil <bmukhiil@gmail.com>
2020-12-06 18:03:45 -0800

    working form box api

--- server/api.py
+++ server/api.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify, json, request
+from language import text_analysis
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def index():
-    return "Index Page"
+@app.route('/textAnalysis', methods=['POST'])
+def emotionInLanguage():
+    if request.method == 'POST':
+        text = request.form['text']
+        textList = [text]
+        analysis = text_analysis(textList)
+        return jsonify(analysis)
 
 
 if __name__ == '__main__':
