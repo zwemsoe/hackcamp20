78751784e3867a3800d356d30a63da76eed615be
zsoe <zwemsoe@gmail.com>
2020-12-06 17:19:26 -0800

    added react

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      isLoading: false,
+      text: "",
+    };
+  }
+
+  handleTextChange = (e) => {
+    this.setState({
+      text: e.target.value,
+    });
+  }
+
+  handleClick = () => {
+
+  }
+
+  
+  render(){
+    return (
+      <div className="App __display-grid-center">
+          <div className="card" style={{boxShadow: "3px 3px 3px #888888"}}>
+            <div className="card-body">
+              <p>Please enter some text:</p>
+              <textarea name="text" rows="4" cols="50" 
+                onChange={this.handleTextChange} value= {this.state.text}/>
+              <br/>
+              <a className="btn btn-secondary" onClick={this.handleClick} href = "javascript:void(0)">
+                Submit
+              </a>
+            </div>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
